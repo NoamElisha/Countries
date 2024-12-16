79d4d0b5b9e059db761e18671daa2c15e6f87ee5
NoamElisha <noampro21@gmail.com>
2024-12-16 23:09:00 +0200

    working on import data from json

---
+++ js/details.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const countryName = urlParams.get('country');
+    
+    fetch('./CountriesData.json', {
+        method: 'GET'
+      }).then((response) => {
+        response.json().then((data) => {
+          console.log(data)
+
+          const country = data.find(c => c.name === countryName)
+
+          const countryDetailsSection = document.querySelector('.country-details');
+
+          countryDetailsSection.innerHTML = `
+            <div class="country-flag">
+            <img src="${country.flag}" alt="${country.name} Flag" />
+          </div>
+          <div class="country-info">
+            <h2 class="country-title">${country.name}</h2>
+            <ul class="country-brief">
+              <li><strong>Population: </strong>${country.population}</li>
+              <li><strong>Region: </strong>${country.region}</li>
+              <li><strong>Capital: </strong>${country.capital}</li>
+            </ul>
+          </div>
+          `;
+        })
+       
+        
+
+       
+
+
+
+
+      }).catch((err) => {
+        console.log(`Error: ${err}` )
+      });
+
+
+});
---
+++ js/index.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('./CountriesData.json', {
+      method: 'GET',
+    })
+      .then((response) => {
+        response.json().then((jsonResponse) => {
+          console.log(jsonResponse);
+  
+          const countriesInDiv = document.querySelector('.countries-grid');
+  
+          if (!countriesInDiv) {
+            console.error("Error: '.countries-grid' element not found in the DOM.");
+            return;
+          }
+  
+          jsonResponse.forEach((country) => {
+            countriesInDiv.innerHTML += `
+              <a
+                href="./details.html?country=${encodeURIComponent(country.name)}"
+                class="country scale-effect"
+                data-country-name="${country.name}"
+              >
+                <div class="country-flag">
+                  <img src="${country.flag}" alt="${country.name} Flag" />
+                </div>
+                <div class="country-info">
+                  <ul class="country-brief">
+                    <li><strong>population: </strong>${country.population}</li>
+                    <li><strong>Region: </strong>${country.region}</li>
+                    <li><strong>capital: </strong>${country.capital}</li>
+                  </ul>
+                </div>
+              </a>
+            `;
+          });
+        });
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+      });
+  });
+  
